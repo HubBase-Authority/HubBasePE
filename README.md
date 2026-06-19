da1d66fa4dcd6a03eb0f5181a46ca8f0c70483ac
Voice659 <237596135+Voice659@users.noreply.github.com>
2026-06-19 09:51:06 +0500

    v0.0.2.0.05-2

---
+++ README.md
@@ -0,0 +1,5 @@
+[![PyPI version](https://badge.fury.io/py/HubBasePE.svg)](https://badge.fury.io/py/HubBasePE)
+# HubBasePE
+## This is HubBasePE.
+# You can use Main.Code() to activate the PE.
+
---
+++ tests/Test6.py
@@ -0,0 +1,20 @@
+print("--- Version Backlog a8-0.0.1[Plan] (default, May 01 2026) ---")
+print("V`s available are 0.0.1a1 - 0.0.1[Plan]")
+print("Exceptions:")
+print("a0.1.0, a0.1.1, a2.7.19")
+print("Newest - 0, start - 0")
+D1 = input("The GIGANTIC update -- ").lower()
+print("0,1,2,3 and so on")
+D2 = input("The bigger update -- ")
+print("0,1,2,3 and so on")
+D3 = input("The update -- ")
+print("a(n) - for alphas, b(n) - for betas, rc(n) - for prereleases, r - for releases")
+D4 = input("Access to Version -- ").lower()
+print(".post(n)`s are not logged!")
+print("")
+if D1 == "0":
+    if D2 == "0":
+        if D3 == "1":
+            if D4 == "a1":
+                print("")
+print("Log canceled")
