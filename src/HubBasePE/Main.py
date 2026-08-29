ac302400dccbccb2bc44e1598467945933547091
Voice659 <237596135+Voice659@users.noreply.github.com>
2026-08-29 11:39:10 +0300

    v0.0.3.0.00b1 - Login system!

--- src/HubBasePE/Main.py
+++ src/HubBasePE/Main.py
@@ -1,9 +1,30 @@
 from . import all_programs
 from .Programs.Manager import Program
-__version__ = "0.0.3.0.00a2"
+__version__ = "0.0.3.0.00b1"
+
+
+class User:
+    def __init__(self, login: str = "", password: str = "1041"):
+        self.VipAccess = False
+        self.username = login
+        self.password = password
+
+    def __str__(self) -> str:
+        return f"{self.username} (pass: {self.password}, VIP: {self.VipAccess})"
+
+    def login(self, *, resetpau: bool = False):
+        if resetpau:
+            self.username, self.password = input("Username -- "), input("Password -- ")
+        self.VipAccess = input("VIP password -- ") == "5280"
+        if not self.VipAccess:
+            print("Incorrect.")
+        else:
+            print("Correct.")
 
 
 def main():
+    user = User()
+    user.login(resetpau=True)
     for pr_id in all_programs:
         try:
             Program(pr_id).run()
--- src/HubBasePE/Programs/__init__.py
+++ src/HubBasePE/Programs/__init__.py
@@ -2,13 +2,3 @@ from pathlib import Path
 
 programs_dir = Path(__file__).resolve().parent
 all_programs = programs_dir.iterdir()
-all_programsl = []
-
-for item in all_programs:
-    if item.is_dir() and item.name != "__pycache__" and Path(item / "main.py").exists():
-        all_programsl.append(item.name)
-
-all_programs = sorted(
-    all_programsl,
-    key=lambda x: (not x.isdigit(), int(x) if x.isdigit() else x)
-)
